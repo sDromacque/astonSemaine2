d1c9ce11cc6c2113f57604ea02dfe5f8c9ebd280
sdromacque <sdromacque@gmail.com>
2016-11-07 14:34:56 +0100

    simple get from json

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
-angular.module()
+angular.module('app', []);
+
+angular.module('app').controller('main', function ($scope, $http) {
+    $http.get('./data/users.json')
+        .then(function(res) {
+            $scope.users = res.data;
+        });
+});
