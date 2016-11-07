865148678631e3fe1448fd438bf7107d374dd963
sdromacque <sdromacque@gmail.com>
2016-11-07 16:48:07 +0100

    Change controller for orderby

--- app/app.js
+++ app/app.js
@@ -1,8 +1,24 @@
 angular.module('app', []);
 
-angular.module('app').controller('main', function ($scope, $http) {
-    $http.get('./data/users.json')
-        .then(function(res) {
-            $scope.users = res.data;
-        });
-});
+angular.module('app')
+    .controller('main', function ($scope, $http) {
+        $scope.sortType = 'name';
+        $scope.sortReverse = false;
+
+        $http.get('./data/users.json')
+            .then(function (res) {
+                $scope.users = res.data;
+            });
+        $scope.showData = true;
+        }
+    )
+    .controller('menu', function ($scope) {
+        $scope.menu = [
+            {label: "Home", url: '/'},
+            {label: "Products", url: '/products'},
+            {label: "Contact", url: '/contact'}
+        ];
+    })
+    .controller('table', function ($scope) {
+        $scope.thead = ['name', 'age', 'address', 'gender'];
+    })
