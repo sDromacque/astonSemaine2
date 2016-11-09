16b2229706a1fb047e34a6cebaed421550c79f5b
sdromacque <sdromacque@gmail.com>
2016-11-09 16:53:23 +0100

    Add leaflet & chartjs

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ["ngRoute"]);
+angular.module('app', ["ngRoute", 'chart.js', "leaflet-directive"]);
 
 angular.module('app')
     .config(function($routeProvider) {
@@ -30,10 +30,77 @@ angular.module('app')
             {label: "Contact", url: '#/contact'}
         ];
     })
-    .controller('homeCtrl', function ($scope) {
-        $scope.toto = "welcome";
 
+    .controller('homeCtrl', function ($scope) {
+        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.series = ['Registred'];
+        $scope.data = [
+            [65, 59, 80, 81, 56, 55, 40]
+        ];
+        $scope.onClick = function (points, evt) {
+            console.log(points, evt);
+        };
+        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+        $scope.options = {
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left'
+                    },
+                    {
+                        id: 'y-axis-2',
+                        type: 'linear',
+                        display: true,
+                        position: 'right'
+                    }
+                ]
+            }
+        }
     })
+
+    .controller('cartoCtrl', ['$scope', function($scope) {
+
+        function getUser(user) {
+            return user._id === '58209960c066406e99d9b9be';
+        }
+        $scope.user = $scope.users.find(getUser);
+
+        // make map
+        angular.extend($scope, {
+            san_fran: {
+                lat: $scope.user.latitude,
+                lng: $scope.user.longitude,
+                zoom: 13
+            },
+            events: {},
+            layers: {
+                baselayers: {
+                    osm: {
+                        name: 'OpenStreetMap',
+                        url: 'https://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png',
+                        type: 'xyz'
+                    }
+                }
+            },
+            defaults: {
+                scrollWheelZoom: false
+            }
+        })
+        $scope.markers = new Array();
+        $scope.$on("leafletDirectiveMap.click", function(event, args){
+            var leafEvent = args.leafletEvent;
+            $scope.markers.push({
+                lat: leafEvent.latlng.lat,
+                lng: leafEvent.latlng.lng,
+                draggable: true
+            });
+        });
+
+    }])
+
     .controller('productCtrl', function ($scope) {
         $scope.sortType = 'name';
         $scope.sortReverse = false;
